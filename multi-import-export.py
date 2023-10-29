707c1277bf0e496646f806f46c77931d9df5359a
dreamsyntax <dreamsyntax@gmail.com>
2023-10-28 18:05:29 -0700

    working mtn

--- multi-import-export.py
+++ multi-import-export.py
@@ -20,12 +20,11 @@ options = {
 }
 
 apply_bone_names = True
-bake_action = True
+bake_action = False
 
-### DFF Import ###
+#### DFF Import ###
 
 dff_importer.import_dff(options)
-
 
 ### BON Import ###
 
@@ -35,9 +34,6 @@ for obj in bpy.data.objects:
     if obj.type == 'ARMATURE':
         obj.select_set(True)
         
-# Ensure you are in Object mode
-bpy.ops.object.mode_set(mode='OBJECT')
-
 arm_obj = bpy.context.view_layer.objects.active
 
 if not arm_obj or type(arm_obj.data) != bpy.types.Armature:
@@ -52,18 +48,14 @@ bpy.ops.object.select_all(action='DESELECT')
 
 def find_bon_root_armature(collection):
     for obj in collection.objects:
-        print("Top Level: " + str(obj))
-        if obj.name == "bon_root":
+        if obj.name == "sh.bon":
             return obj
-        children = [child for child in bpy.context.scene.objects if child.parent == obj]
-        return find_bon_root_armature(children)
     return None
 
 # Start the search from the root level collections
 for collection in bpy.data.collections:
     bon_root_armature = find_bon_root_armature(collection)
     if bon_root_armature:
-        print("FOUND!")
         # Set the armature as the active object
         bpy.context.view_layer.objects.active = bon_root_armature
 
@@ -71,7 +63,7 @@ for collection in bpy.data.collections:
         bon_root_armature.select_set(True)
         break
 
-if not bon_root_armature:
-    bpy.context.window_manager.popup_menu(import_sth_bon.invalid_active_object, title='Error', icon='ERROR')
-
-bpy.ops.import_scene.sth_mtn(filepath=mtn_directory+"\\"+mtns[0], bake_action=bake_action)
+for mtn in mtns:
+    print(mtn_directory+"\\"+mtn)
+    file_path = mtn_directory+"\\"+mtn
+    import_sth_mtn.load(bpy.context, file_path, bake_action)
