7d69663ac62bafe800f763aef803aa98dd95833e
dreamsyntax <dreamsyntax@gmail.com>
2023-11-09 19:26:43 -0700

    Add DFF to GLB with BON for PJS; Unify cleanup()

--- dae-to-glb.py
+++ dae-to-glb.py
@@ -1,10 +1,20 @@
 import bpy
+
 import os
 
 ### Takes a folder full of .dae files and Exports them to .glb
 ### ...using the .dae custom normals
 ### ...with Animations Disabled
 ### ...with Materials Exported
+
+def cleanup():
+    bpy.ops.object.select_all(action='SELECT')
+    bpy.ops.object.delete()
+    for collection in bpy.data.collections:
+        bpy.context.scene.collection.children.unlink(collection)
+        bpy.data.collections.remove(collection)
+    bpy.ops.outliner.orphans_purge(do_local_ids=True, do_linked_ids=True, do_recursive=True)
+
 
 input_folder = "C:\\Users\\user\\Desktop\\__0600"
 output_folder = "C:\\Users\\user\\Desktop\\__0600\\out"
@@ -16,9 +26,7 @@ for dae_file in dae_files:
     input_path = os.path.join(input_folder, dae_file)
     output_path = os.path.join(output_folder, os.path.splitext(dae_file)[0] + '.glb')
 
-    bpy.ops.object.select_all(action='SELECT')
-    bpy.ops.object.delete()
-    bpy.ops.outliner.orphans_purge(do_local_ids=True, do_linked_ids=True, do_recursive=True)
+    cleanup()
 
     bpy.ops.wm.collada_import(filepath=input_path, custom_normals=True)
     
@@ -28,6 +36,4 @@ for dae_file in dae_files:
         export_materials='EXPORT'
     )
     
-bpy.ops.object.select_all(action='SELECT')
-bpy.ops.object.delete()
-bpy.ops.outliner.orphans_purge(do_local_ids=True, do_linked_ids=True, do_recursive=True)
+cleanup()
---
+++ dff-to-glb.py
@@ -0,0 +1,64 @@
+import bpy
+
+import os
+from DragonFF.ops import dff_importer
+from io_scene_sth_mtn import import_sth_bon, import_sth_mtn
+
+### Takes a folder full of .DFF files and Exports them to .glb
+### ...with connect_bones false
+### ...with Animations Disabled
+### ...with Materials Exported
+
+def cleanup():
+    bpy.ops.object.select_all(action='SELECT')
+    bpy.ops.object.delete()
+    for collection in bpy.data.collections:
+        bpy.context.scene.collection.children.unlink(collection)
+        bpy.data.collections.remove(collection)
+    bpy.ops.outliner.orphans_purge(do_local_ids=True, do_linked_ids=True, do_recursive=True)
+
+
+input_folder = "C:\\Users\\user\\Desktop\\GDT"
+output_folder = "C:\\Users\\user\\Desktop\\GDT\\out"
+
+os.makedirs(output_folder, exist_ok=True)
+dae_files = [f for f in os.listdir(input_folder) if f.endswith('.DFF')]
+
+apply_bone_names = True
+bake_action = False
+
+for dae_file in dae_files:
+    input_path = os.path.join(input_folder, dae_file)
+    output_path = os.path.join(output_folder, os.path.splitext(dae_file)[0] + '.glb')
+    cleanup()
+
+    ### DFF Import ###
+
+    options = {
+        'file_name'      : input_path,
+        'image_ext'      : 'PNG',
+        'connect_bones'  : False,
+        'use_mat_split'  : False,
+        'remove_doubles' : False,
+        'group_materials': False,
+        'import_normals' : True
+    }
+
+    dff_importer.import_dff(options)
+    
+    # check if BON exists, and if yes apply it
+    bon_path = os.path.join(input_folder, os.path.splitext(dae_file)[0] + '.BON')
+    if os.path.exists(bon_path):
+        bpy.ops.object.select_all(action='DESELECT')
+        for obj in bpy.data.objects:
+            if obj.type == 'ARMATURE':
+                obj.select_set(True)
+        bpy.ops.import_scene.sth_bon(filepath=bon_path, apply_bone_names=apply_bone_names)
+    
+    bpy.ops.export_scene.gltf(
+        filepath=output_path,
+        export_animations=False,
+        export_materials='EXPORT'
+    )
+
+cleanup()
